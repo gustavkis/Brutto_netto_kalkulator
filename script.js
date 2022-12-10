abb2446da0f3d30b62b544f156d1eca17403e6f2
gustavkis <gustavkis.bp@gmail.com>
2022-12-10 19:33:50 +0100

    megtalal() függvény

--- script.js
+++ script.js
@@ -3,3 +3,18 @@
 function uj() {
     location.reload();
   }
+  function megtalal() {
+    var szamok = azonosit();
+    var tb = szamok[0];
+    kiiras(tb);
+    var SZJA = szamok[1];
+    kiiras1(SZJA);
+    var SZJA0 = szamok[1];
+    kiiras10(SZJA0);
+    var netto = szamok[2];
+    kiiras2(netto);
+    var netto0 = szamok[2];
+    kiiras20(netto0);
+    var mk = szamok[3];
+    kiiras3(mk);
+  }
